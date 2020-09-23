2f41d084e844ba53300bd03a9f2a3164aeaeaf55
github username <github email address>
2020-09-23 19:40:54 -0400

    initial commit

--- app/controllers/scientists_controller.rb
+++ app/controllers/scientists_controller.rb
@@ -1,2 +1,21 @@
 class ScientistsController < ApplicationController
+
+    before_action :find_scientist, only: [:show] #:edit, :update, :destroy
+    
+    def index
+        @scientists = Scientist.all
+    end
+
+    def show
+    end
+
+    def destroy 
+        @scientist.destroy
+    end
+
+    private 
+    def find_scientist
+        @scientist = Scientist.find(params[:id])
+    end 
+
 end
