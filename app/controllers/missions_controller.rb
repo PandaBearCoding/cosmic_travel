6074ce6b3bf52b2c8e86f21d28bd459ba9dbe45c
github username <github email address>
2020-09-23 20:53:29 -0400

    final commit

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,2 +1,22 @@
 class MissionsController < ApplicationController
+
+    def new
+        @mission = Mission.new
+    end
+
+    def create
+        @mission = Mission.create(strong_params)
+        if @mission.valid?
+            redirect_to scientist_path(@mission.scientist_id)
+        else
+            flash[:my_errors] = @mission.errors.full_messages
+            render :new
+        end
+    end
+
+    private 
+
+    def strong_params
+        params.require(:mission).permit(:name, :scientist_id, :planet_id)
+    end
 end
--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,4 +1,8 @@
 class Mission < ApplicationRecord
     belongs_to :scientist
     belongs_to :planet
+
+    validates :name, presence: true, uniqueness: {case_sensitive: false, message: " not available!"}
+    validates :scientist_id, presence: true
+    validates :planet_id, presence: true
 end
